7241d346059454db33f5d1966ede8e2729211d3c
Felix Yin <lafenglafenghaha@163.com>
2017-04-01 17:01:55 +0800

    提交修改

---
+++ TestCocoapodsProject/Example/TestCocoapodsProject/CocoapodsTest/FYCocoapodsViewController.h
@@ -0,0 +1,13 @@
+//
+//  FYCocoapodsViewController.h
+//  TestCocoapodsProject
+//
+//  Created by Felix Yin on 2017/4/1.
+//  Copyright © 2017年 Felix Yin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FYCocoapodsViewController : UIViewController
+
+@end
---
+++ TestCocoapodsProject/Example/TestCocoapodsProject/CocoapodsTest/FYCocoapodsViewController.m
@@ -0,0 +1,37 @@
+//
+//  FYCocoapodsViewController.m
+//  TestCocoapodsProject
+//
+//  Created by Felix Yin on 2017/4/1.
+//  Copyright © 2017年 Felix Yin. All rights reserved.
+//
+
+#import "FYCocoapodsViewController.h"
+
+@interface FYCocoapodsViewController ()
+
+@end
+
+@implementation FYCocoapodsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
